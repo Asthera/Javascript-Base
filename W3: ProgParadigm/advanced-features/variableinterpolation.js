0b48d0022a5a2a537be99fe338caf102b5c10122
4oli <h>
2023-02-09 23:05:29 +0100

    End

--- W3: ProgParadigm/advanced-features/variableinterpolation.js
+++ W3: ProgParadigm/advanced-features/variableinterpolation.js
@@ -1,11 +1,3 @@
 let first = "Hel";
 let end = "orld !!";
 console.log(`${first}lo, w${end}`);
-
-function animalCan() {
-  birdCan();
-  for (a of Object.entries(animal)) {
-    console.log(a[0] + ": " + a[1]);
-  }
-}
-birdCan();
