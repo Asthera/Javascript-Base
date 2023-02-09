c4aebd6b82d715c50d2bd8a2df72a6b1b8e0faae
4oli <h>
2023-02-09 10:17:38 +0100

    Rename W2

--- W3:ProgParadigm/object-oriented-programing/example.js
+++ W3:ProgParadigm/object-oriented-programing/example.js
@@ -2,8 +2,9 @@ var purchase = {
   shoes: 50,
   statetax: 1.2,
   totalPrice: function () {
+    // The "this" keyword is an alias for the name of the object.
     let calculation = this.shoes * this.statetax;
     console.log(calculation);
   },
 };
-purchase.totalPrice();
+purchase.totalPrice(); // 60
