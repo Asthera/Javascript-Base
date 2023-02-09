87c448dfae09e16b2c7e75e5bb665d2b05a2a90d
4oli <h>
2023-02-09 10:47:15 +0100

    Update classes

--- W3:ProgParadigm/object-oriented-programing/classes.js
+++ W3:ProgParadigm/object-oriented-programing/classes.js
@@ -12,3 +12,30 @@ class Car {
 
 var Maybach = new Car("Red", 100);
 Maybach.turboOn();
+
+class Train {
+  constructor(color, lightsOn) {
+    this.color = color;
+    this.lightsOn = lightsOn;
+  }
+  toggleLights() {
+    this.lightsOn = !this.lightsOn;
+  }
+  lightsStatus() {
+    console.log("Lights on?", this.lightsOn);
+  }
+  getSelf() {
+    console.log(this);
+  }
+  getPrototype() {
+    var proto = Object.getPrototypeOf(this);
+    console.log(proto);
+  }
+}
+
+var train4 = new Train("red", false);
+
+train4.toggleLights(); // undefined
+train4.lightsStatus(); // Lights on? true
+train4.getSelf(); // Train {color: 'red', lightsOn: true}
+train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
